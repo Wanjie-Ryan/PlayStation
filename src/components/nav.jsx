4617f6d882855bd97a63b3eedb8697499e8e6fc4
Ryan <ryanwanjiedistortion@gmail.com>
2023-03-26 06:33:20 +0300

    start

---
+++ src/components/nav.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import './nav.css'
+
+
+function nav() {
+
+
+
+
+  return (
+
+
+    <>
+    
+
+        <nav className='main'>
+
+
+
+
+
+        </nav>
+    
+    
+        
+
+
+
+    </>
+
+
+  )
+}
+
+export default nav
