fafebe3ba35157a22b02781bdff47b00aae152dd
Ryan <ryanwanjiedistortion@gmail.com>
2023-03-28 20:22:12 +0300

    games

--- src/components/gaming/samplegames.jsx
+++ src/components/gaming/samplegames.jsx
@@ -24,8 +24,27 @@ function Samplegames() {
 
             id:2,
             name: 'Resident Evil 4',
-            img:'../../Pics/resident evil.jpg'
+            img:'../../Pics/resident evil.jpg',
+            experience:'Survival is just the beginning',
+            description:'Experience a thrilling reimagining of the action-horror classic, featuring modernized gameplay, a reimagined story & spectacular visuals'
+        },
+
+        {
+            id:3,
+            name:'DIABLO IV',
+            img:'../../Pics/diablo.webp',
+            experience:'Breakdown',
+            description:'Play the Diablo IV open beta and face ceaseless demons in nightmarish dungeons in a vast open world from March 24th – 26th'
+        },
+
+        {
+            id:4,
+            name:'Sun Squad',
+            img:'../../Pics/horizon.jpg',
+            experience:`Sun's out? Guns out!`,
+            description:`Play the new Heatwave limited-time mode and unlock 24 limited-time cosmetics. Collect all 24 to receive Ash's new Heirloom!`
         }
+
 
 
 
