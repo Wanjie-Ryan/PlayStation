c0e7591aa868351f1b1a6c4173553bb21ce38743
Ryan <ryanwanjiedistortion@gmail.com>
2023-03-28 07:06:29 +0300

    games

--- src/components/gaming/samplegames.jsx
+++ src/components/gaming/samplegames.jsx
@@ -17,14 +17,27 @@ function Samplegames() {
 
             <section className="main-games">
 
+                <div className="content">
 
 
 
+                        <div className="tchia">
+                            <p>Tchia</p>
+                        </div>
+
+                        <div className="exp">
+                            <p>A unique tropical adventure</p>
+                        </div>
 
 
+                        <div className="desc">
+                            <p>Prepare to climb, swim, glide and sail your way around a beautiful open-world, available now with PlayStation Plus Extra & Premium tiers</p>
+                        </div>
+
+                        <button className="more"> Find out more </button>
 
 
-
+                </div>
 
 
 
