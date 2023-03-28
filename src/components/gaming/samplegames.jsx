0b5dc49e00c3a56a8f4f7227f01d70f3808537e2
Ryan <ryanwanjiedistortion@gmail.com>
2023-03-28 20:38:22 +0300

    games

--- src/components/gaming/samplegames.jsx
+++ src/components/gaming/samplegames.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './samplegames.css'
-import Tchia from '../../Pics/Tchia.webp'
+
 
 
 
@@ -55,41 +55,50 @@ function Samplegames() {
 
     <>
 
-        { games.map(())  <main className="main-sec">
-
-            <div className="main-games">
-
-                <img src={Tchia} alt="games" className="img" />
-
-            </div>
+        { games.map(({id, name, img,experience, description })=>{
             
-
-                <div className="content">
-
+            return(
 
 
-                        <div className="tchia">
-                            <p>Tchia</p>
-                        </div>
-
-                        <div className="exp">
-                            <p>A unique tropical adventure</p>
-                        </div>
-
-
-                        <div className="desc">
-                            <p>Prepare to climb, swim, glide and sail your way around a beautiful open-world, available now with PlayStation Plus Extra & Premium tiers</p>
-                        </div>
-
-                        <button className="more"> Find out more </button>
-
-
+              <main className="main-sec" key ={id}>
+            
+                <div className="main-games">
+            
+                    <img src={img} alt="games" className="img" />
+            
                 </div>
+                
+            
+                    <div className="content">
+            
+            
+            
+                            <div className="tchia">
+                                <p>{name}</p>
+                            </div>
+            
+                            <div className="exp">
+                                <p>{experience}</p>
+                            </div>
+            
+            
+                            <div className="desc">
+                                <p>{description}</p>
+                            </div>
+            
+                            <button className="more"> Find out more </button>
+            
+            
+                    </div>
+            
+            
+            
+            
+            </main>)})}
 
 
 
-
-        </main>}
+            
 
 
 
