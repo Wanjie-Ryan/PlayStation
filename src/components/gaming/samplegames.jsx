76cdf3d5871e7263052fc736cb111cba0a2e67da
Ryan <ryanwanjiedistortion@gmail.com>
2023-04-04 18:49:05 +0300

    end

--- src/components/gaming/samplegames.jsx
+++ src/components/gaming/samplegames.jsx
@@ -1,58 +1,58 @@
 import React, {useState} from 'react'
 import './samplegames.css'
-import { Navigation, Pagination } from 'swiper';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+// import { Navigation, Pagination } from 'swiper';
+// import { Swiper, SwiperSlide } from 'swiper/react';
+// import 'swiper/css';
+// import 'swiper/css/navigation';
+// import 'swiper/css/pagination';
 import resEvil from '../../Pics/resident evil.jpg'
 
 
 function Samplegames() {
 
 
-    const [games, setgames]  =useState( [
+    // const [games, setgames]  =useState( [
 
 
-        {
-            id:1,
-            name: 'Tchia',
-            img: '../../Pics/Tchia.webp',
-            experience: 'A unique tropical adventure',
-            description:'Prepare to climb, swim, glide and sail your way around a beautiful open-world, available now with PlayStation Plus Extra & Premium tiers'
+    //     {
+    //         id:1,
+    //         name: 'Tchia',
+    //         img: '../../Pics/Tchia.webp',
+    //         experience: 'A unique tropical adventure',
+    //         description:'Prepare to climb, swim, glide and sail your way around a beautiful open-world, available now with PlayStation Plus Extra & Premium tiers'
 
 
-        },
+    //     },
 
-        {
+    //     {
 
-            id:2,
-            name: 'Resident Evil 4',
-            img:'../../Pics/resident evil.jpg',
-            experience:'Survival is just the beginning',
-            description:'Experience a thrilling reimagining of the action-horror classic, featuring modernized gameplay, a reimagined story & spectacular visuals'
-        },
+    //         id:2,
+    //         name: 'Resident Evil 4',
+    //         img:'../../Pics/resident evil.jpg',
+    //         experience:'Survival is just the beginning',
+    //         description:'Experience a thrilling reimagining of the action-horror classic, featuring modernized gameplay, a reimagined story & spectacular visuals'
+    //     },
 
-        {
-            id:3,
-            name:'DIABLO IV',
-            img:'../../Pics/diablo.webp',
-            experience:'Breakdown',
-            description:'Play the Diablo IV open beta and face ceaseless demons in nightmarish dungeons in a vast open world from March 24th – 26th'
-        },
+    //     {
+    //         id:3,
+    //         name:'DIABLO IV',
+    //         img:'../../Pics/diablo.webp',
+    //         experience:'Breakdown',
+    //         description:'Play the Diablo IV open beta and face ceaseless demons in nightmarish dungeons in a vast open world from March 24th – 26th'
+    //     },
 
-        {
-            id:4,
-            name:'Sun Squad',
-            img:'../../Pics/horizon.jpg',
-            experience:`Sun's out? Guns out!`,
-            description:`Play the new Heatwave limited-time mode and unlock 24 limited-time cosmetics. Collect all 24 to receive Ash's new Heirloom!`
-        }
+    //     {
+    //         id:4,
+    //         name:'Sun Squad',
+    //         img:'../../Pics/horizon.jpg',
+    //         experience:`Sun's out? Guns out!`,
+    //         description:`Play the new Heatwave limited-time mode and unlock 24 limited-time cosmetics. Collect all 24 to receive Ash's new Heirloom!`
+    //     }
 
 
-    ])
+    // ])
 
-    console.log(games)
+    // console.log(games)
 
 
 
@@ -62,23 +62,16 @@ function Samplegames() {
     <>
                 <main className="main-sec">
 
-                        {/* { games.map((items)=>{ */}
-                            
-                           
+                                <div className="components"  >
 
-                                <section className="components"  >
-
+                                
                                         <div className="main-games">
                                     
                                             <img src={resEvil} alt="games" className="img" />
                                     
                                         </div>
 
-                                        {/* mapping elements */}
-
-                                        {/* mapping has problem */}
                                         
-                                    
                                             <div className="content">
                                     
                                                     <div className="tchia">
@@ -99,23 +92,15 @@ function Samplegames() {
                                     
                                             </div>
                                 
-                                </section>
-                        
-                </main>
+                                    
 
-
-                
+                                </div>
+                </main>         
     </>
-
-
-
 
   )
 
 
-
-
-
-}
+  }
 
 export default Samplegames
